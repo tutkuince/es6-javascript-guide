ac9489aa991eadfcd649785e3e3bae5272867aa4
tutkuince <tutku.ince@outlook.com>
2020-09-28 21:56:04 +0300

    sample js with basic addition

---
+++ 01theforeachhelper/sample.js
@@ -0,0 +1,13 @@
+// create an array of numbers
+let numbers = [1, 2, 3, 4, 5];
+
+// Create a variable to hold the sum
+let sum = 0;
+
+// Loop over the array, incrementing the sum variable
+numbers.forEach(function (number) {
+    sum += number;
+});
+
+// print the sum variable
+console.log(sum);
