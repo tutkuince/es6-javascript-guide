ec2cb85b1558bb63fb7d0e469fb8d74b5532e9dc
tutkuince <tutku.ince@outlook.com>
2020-09-28 21:58:38 +0300

    adder function has been added.

--- 01theforeachhelper/sample.js
+++ 01theforeachhelper/sample.js
@@ -4,10 +4,15 @@ let numbers = [1, 2, 3, 4, 5];
 // Create a variable to hold the sum
 let sum = 0;
 
-// Loop over the array, incrementing the sum variable
-numbers.forEach(function (number) {
+function adder(number) {
     sum += number;
-});
+}
+
+// Loop over the array, incrementing the sum variable
+// numbers.forEach(function (number) {
+//     sum += number;
+// });
+numbers.forEach(adder)
 
 // print the sum variable
 console.log(sum);
