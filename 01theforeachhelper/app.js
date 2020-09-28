1ba455745fb4d508456f5c9feee09a6d2188aa57
tutkuince <tutku.ince@outlook.com>
2020-09-28 21:50:31 +0300

    iterate with forEach helper

--- 01theforeachhelper/app.js
+++ 01theforeachhelper/app.js
@@ -3,3 +3,8 @@ let colors = ['red', 'blue', 'green'];
 for (let i = 0; i < colors.length; i++) {
     console.log(colors[i]);
 }
+console.log('------------------')
+
+colors.forEach(function (color) {
+    console.log(color);
+});
