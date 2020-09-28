9154f1845727300160ecde30bbd8f86bd44873cc
tutkuince <tutku.ince@outlook.com>
2020-09-28 22:11:10 +0300

    exercise 1

---
+++ 01theforeachhelper/exercise1.js
@@ -0,0 +1,17 @@
+function handlePosts() {
+    let posts = [
+        { id: 23, title: 'Daily JS News' },
+        { id: 52, title: 'Code Refactor City' },
+        { id: 105, title: 'The Brightest Ruby' }
+    ];
+
+    posts.forEach(function (post) {
+        savePost(post);
+    });
+
+    function savePost(post) {
+        console.log('Id: ' + post.id + ' Title: ' + post.title);
+    }
+}
+
+handlePosts();
