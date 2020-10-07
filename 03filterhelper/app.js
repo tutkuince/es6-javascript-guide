34468cf5a3195bc0b966c03ec2250c93df932b90
tutkuince <tutku.ince@outlook.com>
2020-10-07 17:45:52 +0300

    sample without filter

---
+++ 03filterhelper/app.js
@@ -0,0 +1,16 @@
+let products = [
+    {name: 'cucumber', type: 'vegetable'},
+    {name: 'banana', type: 'fruit'},
+    {name: 'celery', type: 'vegetable'},
+    {name: 'orange', type: 'fruit'},
+];
+
+let filteredProducts = [];
+
+for (let i = 0; i < products.length; i++) {
+    if (products[i].type === 'fruit') {
+        filteredProducts.push(products[i]);
+    }
+}
+
+console.log(filteredProducts);
